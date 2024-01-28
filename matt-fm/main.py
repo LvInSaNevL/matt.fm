6e6850bacd263c2932e36d40ae32b569083f7ad6
Matt Wollam <mattwollam@gmail.com>
2024-01-28 14:56:04 -0500

    Fixed auth issues
    We are now using actual account passwords instead of the generic ones

--- matt-fm/main.py
+++ matt-fm/main.py
@@ -8,19 +8,19 @@ import time
 def main():    
     # Getting reddit data
     reddit.authenticate()
-    reddit.getPosts(10)
+    reddit.getPosts(5)
 
     # Cleaning out the existing playlists
-    youtube.remove_from_playlist()
+    # youtube.remove_from_playlist()
     
     # You need this sleep for YouTube to catch up, it could probably be reduced but this is safe
     data = db_hook.todaySongs
     time.sleep(5)
     
-    # Adding songs to the YT playlist
-    for c in data:
-        print(c)
-        youtube.add_to_playlist(c.song.yt_id)
+    # # Adding songs to the YT playlist
+    # for c in data:
+    #     print(c)
+    #     youtube.add_to_playlist(c.song.yt_id)
 
     # Adding songs to the YT database
     db_hook.updateDB()
